e2de92753f38263f3e6312d39ab418dd53184755
Marcie DeFonce <marcie.defonce@gmail.com>
2023-04-20 16:54:55 -0500

    10.5. Exercises: Controllers and Routing

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -26,8 +26,9 @@ public class HelloController {
 
     // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, @RequestParam String language) {
+        String greeting = HelloController.createMessage(name, language);
+        return "<h3 style='color:green'>" + greeting + "</h3>";
     }
 
     // Handles requests of the form /hello/LaunchCode
@@ -43,10 +44,25 @@ public class HelloController {
                 "<body>" +
                 "<form action = '/hello' method = 'post'>" + // submit a request to /hello
                 "<input type = 'text' name = 'name' >" +
+                "<select name='language'>" +
+                "    <option value='English'>English</option>" +
+                "    <option value='German'>German</option>" +
+                "    <option value='Spanish'>Spanish</option>" +
+                "    <option value='French'>French</option>" +
+                "    <option value='Klingon'>Klingon</option>" +
+                "</select>" +
                 "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+    }
+
+    public static String createMessage(String name, String language) {
+        if (language.contains("German")) {return "Hallo, " + name + "!";}
+        else if (language.contains("French")) {return "Bonjour, " + name + "!";}
+        else if (language.contains("Spanish")) {return "Hola, " + name + "!";}
+        else if (language.contains("Klingon")) {return "nuqneH, " + name + "!";}
+        else {return "Hello, " + name + "!";}
     }
 
 }
